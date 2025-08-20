ee2711204a4648e5f0f0ef553e09622f438e14ee
Julia <juliacostacarvalho0928@gmail.com>
2025-08-20 14:25:40 -0300

    duplicando o botao cadastrar

--- Projeto1/frmMenu.Designer.cs
+++ Projeto1/frmMenu.Designer.cs
@@ -32,6 +32,7 @@
             this.btnCadastro = new System.Windows.Forms.Button();
             this.btnSair = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblMenu
@@ -47,7 +48,7 @@
             // btnCadastro
             // 
             this.btnCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCadastro.Location = new System.Drawing.Point(136, 87);
+            this.btnCadastro.Location = new System.Drawing.Point(136, 95);
             this.btnCadastro.Name = "btnCadastro";
             this.btnCadastro.Size = new System.Drawing.Size(148, 37);
             this.btnCadastro.TabIndex = 1;
@@ -75,11 +76,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(136, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Cadastro";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(442, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnCadastro);
@@ -98,5 +110,6 @@
         private System.Windows.Forms.Button btnCadastro;
         private System.Windows.Forms.Button btnSair;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
